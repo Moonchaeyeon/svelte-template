a0e6ff35488bc0478a79e32dae467e1a77c3aad5
moonchaeyeon <sso07069@gmail.com>
2023-09-06 12:43:39 +0900

    [chore] Add tailwind to storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -6,7 +6,17 @@ const config: StorybookConfig = {
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
-    "@storybook/addon-mdx-gfm"
+    "@storybook/addon-mdx-gfm",
+    {
+      name: "@storybook/addon-styling",
+      options: {
+        // Check out https://github.com/storybookjs/addon-styling/blob/main/docs/api.md
+        // For more details on this addon's options.
+        postCss: {
+          implementation: require.resolve("postcss"),
+        },
+      },
+    },
   ],
   framework: {
     name: "@storybook/sveltekit",
--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,4 +1,5 @@
 import type { Preview } from "@storybook/svelte";
+import "../src/styles.css";
 
 const preview: Preview = {
   parameters: {
